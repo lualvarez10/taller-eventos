bf0f847cb837e63b98dc92e1654e89a7c8fad867
lualvarez10 <lu_mjfan95@hotmail.com>
2023-08-15 17:26:41 -0300

    solucion-click

--- eventos.js
+++ eventos.js
@@ -2,19 +2,25 @@
 const myDiv = document.createElement('div');
 const myButton = document.getElementById("Butt");
 
-function handleClick() {
 
+function handleClick() {
 myDiv.appendChild(myButton);
 document.body.appendChild(myDiv);
 }
 
-myButton.addEventListener('click', handleClick);
+myButton.addEventListener('click', function(event) {
+    event.stopPropagation(); // Detener la propagación del evento solo en el botón
+    handleClick();
+});
 
 // Modificar estilo del div
 myDiv.style.padding = '5px';
 myDiv.style.backgroundColor = 'green';
 
+
 //Evento al hacer click en el div
-myDiv.addEventListener("click", function() {
-    alert("Hola! Soy el div");
+myDiv.addEventListener("click", function(event) {
+    if (event.target === myDiv) {
+        alert("Hola! Soy el div");
+    }
 });
